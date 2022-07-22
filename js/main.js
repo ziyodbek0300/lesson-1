0ba5c5d707ee09ec9d6da788104b4cd8335d5475
ziyodbek0300 <mr.prgrmr03@gmail.com>
2022-07-22 07:15:08 +0500

    pure reactjs project created

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+const wrapper = React.createElement('div', {className: "wrapper"}, [
+    React.createElement('h1', null, 'Salom Dunyo!'),
+    React.createElement('h1', null, 'Salom Dunyo!')
+]);
+
+root.render(wrapper);
